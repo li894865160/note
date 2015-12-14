3277d44fb646e6753c2d0249cc8945de3a85f265
baixiaozheng <baixiaozheng@163.com>
2015-12-15 01:18:17 +0800

    添加类别的方法

--- dao/src/main/java/com/note/dao/note/CategoryDao.java
+++ dao/src/main/java/com/note/dao/note/CategoryDao.java
@@ -1,12 +1,14 @@
 package com.note.dao.note;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 import com.note.dao.BaseDao;
 import com.note.model.note.Category;
 
+@SuppressWarnings("unchecked")
 @Repository(value="categoryDao")
 public class CategoryDao extends BaseDao {
 
@@ -19,10 +21,31 @@ public class CategoryDao extends BaseDao {
 		return super.list();
 	}
 	
-	@SuppressWarnings("unchecked")
 	public List<Category> list(){
 		String sql = "select * from category where enable=true";
 		return (List<Category>) super.listEntityWithSql(sql);
+	}
+	
+	public Category add(Category category){
+		return super.add(category);
+	}
+	
+	public Category getById(int id){
+		return super.get(id);
+	}
+	
+	public Category update(Category category){
+		return super.update(category);
+	}
+	
+	public List<Category> listByUserId(int userId){
+		String sql = "select * from category where enable=:enable ";
+		Map<String, Object> map = configMap(new String[]{"enable"}, new Object[]{true});
+		if(userId > 0){
+			sql += "and user_id=:userId";
+			map.put("userId", userId);
+		}
+		return (List<Category>) super.listEntityWithSql(sql, map);
 	}
 }
  
