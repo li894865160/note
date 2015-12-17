2b693aee8299850043d66bf04e13cc3c3d810a09
baixiaozheng <baixiaozheng@com>
2015-12-17 18:58:04 +0800

    文档更新

--- web/src/main/java/com/note/web/controller/note/NoteController.java
+++ web/src/main/java/com/note/web/controller/note/NoteController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.note.common.HTTPCodeStatus;
@@ -35,11 +36,10 @@ public class NoteController extends BaseController {
         return returnSuccess(HTTPCodeStatus.HTTPCODE_OK);
     }
     
-    
     @ResponseBody
-    @RequestMapping(value="test")
+    @RequestMapping(value="test", method = RequestMethod.GET)
     @Authority(type = AuthorityType.SECURITY)
-    private ResponseEntity test(){
+    private ResponseEntity testPut(){
         //SecurityUtil.setSecurityUser(null);
         return returnSuccess(HTTPCodeStatus.HTTPCODE_OK, null, HTTPCodeStatus.HTTPCODE_OK_MESSAGE);
     }
